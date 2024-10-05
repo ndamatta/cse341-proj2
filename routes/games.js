b1153c8560c9aac47e514659395551c4bd79d01d
ndamatta <damatta.natanael@gmail.com>
2024-10-05 10:41:25 -0300

    add validation rules

--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const gamesController = require('../controllers/games')
+const { createGameRules, updateGameRules, validate } = require('../validators/gameValidator')
+
 
 router.get('/', gamesController.getAllGames);
 router.get('/:id', gamesController.getSingleGame);
 
-router.post('/', gamesController.createGame);
-router.put('/:id', gamesController.updateGame);
+router.post('/', validate(createGameRules()), gamesController.createGame);
+router.put('/:id', validate(updateGameRules()),gamesController.updateGame);
 router.delete('/:id', gamesController.deleteGame)
 
 module.exports =  router;
