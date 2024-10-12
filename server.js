a7e79ca06b9b52cdb7055f136726d81c701e7a26
ndamatta <damatta.natanael@gmail.com>
2024-10-12 11:08:09 -0300

    set up app.use again with passport

--- server.js
+++ server.js
@@ -2,24 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const passport = require('passport');
-const indexRoute = require('./routes');
 const GithubStrategy = require('passport-github2').Strategy;
+const cors = require('cors'); 
+const indexRoute = require('./routes');
+
+
 
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+
 app
   .use(bodyParser.json())
+  .use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "POST, GET, PUT, PATCH, OPTIONS, DELETE"
+    );
     next();
   })
-  .use('/', indexRoute);
-
-process.on('uncaughtException', (err, origin) => {
-  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`)
-});
+  .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+  .use(cors({ origin: '*' })) 
+  .use("/", require("./routes/index.js"));
 
 mongodb.connectDatabase((err) => {
   if (err) {
