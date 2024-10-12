85f3f3016982440b7f946632199188fc191e0b42
ndamatta <damatta.natanael@gmail.com>
2024-10-12 11:22:59 -0300

    setup passport and github oapp

--- server.js
+++ server.js
@@ -38,6 +38,32 @@ app
   .use(cors({ origin: '*' })) 
   .use("/", require("./routes/index.js"));
 
+  passport.use(new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  ));
+
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  passport.deserializeUser((user, done) => {
+    done(null, user)
+  });
+
+  app.get('/', (req, res) => {res.send(req.session.user != undefined ? `Logged in as: ${req.session.user.displayName}`: "Logged Out")});
+  app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false}),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  });
+
 mongodb.connectDatabase((err) => {
   if (err) {
     console.log(err);
